32ba7ea7f2f8278417c26914fb6e258478392dbc
Marco Lauria <72135329+Avocadobeef@users.noreply.github.com>
2020-11-30 19:11:17 +0100

    boxplot update

--- bin/boxplot.py
+++ bin/boxplot.py
@@ -7,7 +7,7 @@ import glob
 def main():
     # todo: load the "results.csv" file from the mia-results directory
     # todo: read the data into a list
-    # todo: plot the Dice coefficients per label (i.e. white matter, gray matter, hippocampus, amygdala, thalamus) in a boxplot
+    # todo: plot the Dice coefficients per label (i.e. white matter, gray matter, hippocampus, etc) in a boxplot
 
     # alternative: instead of manually loading/reading the csv file you could also use the pandas package
     # but you will need to install it first ('pip install pandas') and import it to this file ('import pandas as pd')
@@ -15,10 +15,13 @@ def main():
 
     os.chdir('mia-result/')
     result_path = glob.glob('*/results.csv')[0]
-    seg_path = glob.glob('*/117122_SEG-PP.mha')[0]
+    print(result_path)
+    #seg_path = glob.glob('*/117122_SEG-PP.mha')[0]
 
     result = pd.read_csv(result_path, ";")
     boxplot = result.boxplot(column='DICE', by="LABEL")
+    plt.show()
+    boxplot = result.boxplot(column='HDRFDST', by="LABEL")
     plt.show()
 
 if __name__ == '__main__':
