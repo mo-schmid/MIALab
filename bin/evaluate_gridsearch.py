d94e24bccc7001af944423af1b0bb17e21d3f8d0
mo-schmid <moritz-schmid@outlook.com>
2020-12-29 16:04:36 +0100

    cleaned up code

--- bin/evaluate_gridsearch.py
+++ bin/evaluate_gridsearch.py
@@ -23,7 +23,7 @@ def main(result_dir: str):
     # create a list with the path to all subdirectories
     dir_list = [x for x in result_dir.iterdir() if x.is_dir()]
 
-
+    # load all data in a data frame
     dfs = pd.DataFrame()
     methods = []
     for dir in dir_list:
@@ -39,20 +39,20 @@ def main(result_dir: str):
         dfs = dfs.append(new_data, ignore_index=True)
 
 
-
+    # rank the data by dice coefficient
     group_DICE = dfs[dfs.STATISTIC == 'MEAN'][dfs.METRIC == 'DICE'].groupby(list(parameter.keys()))[
         'VALUE'].mean()
     group_DICE = group_DICE.sort_values(ascending=False)
     group_DICE = pd.DataFrame(group_DICE)
     group_DICE.insert(group_DICE.shape[1], 'RANK_DICE', np.arange(group_DICE.shape[0]))
 
-
+    # rank data by hausdorff distance
     group_HDRFDST = dfs[dfs.STATISTIC == 'MEAN'][dfs.METRIC == 'HDRFDST'].groupby(list(parameter.keys()))['VALUE'].mean()
     group_HDRFDST  = group_HDRFDST.sort_values(ascending=True)
     group_HDRFDST = pd.DataFrame(group_HDRFDST)
     group_HDRFDST.insert(group_HDRFDST.shape[1], 'RANK_HDRFDST', np.arange(group_HDRFDST.shape[0]))
 
-
+    # compute mean rank
     combined = pd.concat([group_DICE, group_HDRFDST], axis=1)
     combined['MEAN_RANK'] = combined[['RANK_DICE', 'RANK_HDRFDST']].mean(axis=1)
     combined = combined.sort_values(by='MEAN_RANK', ascending=True)
@@ -64,7 +64,6 @@ def main(result_dir: str):
 
 
 
-
 if __name__ == '__main__':
     """The program's entry point.
 
@@ -72,12 +71,6 @@ if __name__ == '__main__':
     """
     parser = argparse.ArgumentParser(description='Result plotting.')
 
-    # parser.add_argument(
-    #     '--result_dir',
-    #     type=str,
-    #     default='./mia-result/2020-11-23-13-51-20',
-    #     help='Path to the result dir.'
-    # )
 
     # # result folder of gridsearch of random forest
     # parser.add_argument(
@@ -94,8 +87,6 @@ if __name__ == '__main__':
         default='./mia-result/gridsearch_PKF/2020-12-11-09-51-54/with_PP',
         help='Path to the result dir.'
     )
-
-
 
 
     parser.add_argument(
